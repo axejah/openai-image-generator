d2aef70b1092a19c2cf621db86dfc3bfdc595458
axejah <kevin.soester1@gmail.com>
2022-12-11 03:00:20 +0100

    noreferrer added to anchor tags

--- components/Footer.js
+++ components/Footer.js
@@ -7,13 +7,17 @@ function Footer() {
         <div className={styles.container}>
           <div>
             created with <span style={{ color: 'red' }}>♥️</span> by{' '}
-            <a href="https://www.github.com/axejah" target="_blank">
+            <a
+              href="https://www.github.com/axejah"
+              target="_blank"
+              rel="noreferrer"
+            >
               Kevin
             </a>
           </div>
           <div>
             built upon{' '}
-            <a href="https://openai.com/" target="_blank">
+            <a href="https://openai.com/" target="_blank" rel="noreferrer">
               openai.com
             </a>
           </div>
